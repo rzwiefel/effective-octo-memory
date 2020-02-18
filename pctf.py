d84e37cee127cd14c5f18609f8cb290e2d50172b
ReubenFernand35 <reubenprakashfernandes@gmail.com>
2020-02-17 19:55:21 -0600

    Initial Wrapper / Portscan
    Added a python wrapper to run each script from centralized location. Also added initial portscanner that attempts to find all ip's on network and scan all ports.

---
+++ pctf.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import os
+
+choice = int(input("Team 5 PCTF Code\nPlease choose from the menu\n1: Scan Victim\n"))
+
+if(choice == 1):
+	os.system("python scripts/portscan.py")
+else:
+	print("Invalid choice")
---
+++ scripts/portscan.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import nmap
+
+print('Scanning...')
+nm = nmap.PortScanner()
+nm.scan('10.0.2.0/24')
+
+for host in nm.all_hosts():
+	print('IP Address: %s, Hostname: %s' %(host, nm[host].hostname()))
+	print('State: %s\n' % nm[host].state())
+	for protocol in nm[host].all_protocols():
+		print('Protocol: %s' % protocol)
+		ports = nm[host][protocol].keys()
+		ports.sort()
+		for port in ports:
+			print('port: %s\tstate: %s' % (port, nm[host][protocol][port]['state']))
